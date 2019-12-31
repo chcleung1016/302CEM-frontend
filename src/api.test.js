a937158e4cc9d66cf01b751630ee605c6336f430
Ken Ngan <197104101@stu.vtc.edu.hk>
2020-01-01 07:45:59 +0800

    fixed api unit test

--- src/api.test.js
+++ src/api.test.js
@@ -1,9 +1,13 @@
+import source from "../db.json";
 import { getUser } from "./api";
-import user from "./test.json";
-describe("testing api", () => {
-  it("getUser()", () => {
-    getUser("getUser(test)").then(res => {
-      expect(getUser("test")).toEqual(user);
-    });
+var nock = require("nock");
+describe("getUser()", function() {
+  it("getUser('AgileTest')", async function() {
+    nock("http://localhost:5000")
+      .defaultReplyHeaders({ "access-control-allow-origin": "*" })
+      .get("/AgileTest")
+      .reply(200, source);
+    const res = await getUser("AgileTest");
+    expect(res).toEqual(source);
   });
 });
