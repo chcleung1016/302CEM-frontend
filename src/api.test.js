5e9e908a73f28dd88cb821a3744a8388f2dc202c
Ken Ngan <197104101@stu.vtc.edu.hk>
2020-01-01 09:10:52 +0800

    api.test.js updated

--- src/api.test.js
+++ src/api.test.js
@@ -2,12 +2,13 @@ import source from "../db.json";
 import { getUser } from "./api";
 var nock = require("nock");
 describe("getUser()", function() {
-  it("getUser('AgileTest')", async function() {
-    nock("http://localhost:5000")
-      .defaultReplyHeaders({ "access-control-allow-origin": "*" })
-      .get("/AgileTest")
-      .reply(200, source);
+  nock("http://localhost:5000")
+    .defaultReplyHeaders({ "access-control-allow-origin": "*" })
+    .get("/AgileTest")
+    .reply(200, source);
+  it("getUser('AgileTest') should return db.json", async function() {
     const res = await getUser("AgileTest");
     expect(res).toEqual(source);
+    expect(res.name).toEqual(source.name);
   });
 });
